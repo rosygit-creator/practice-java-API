40feb6e63fc5e1173ceb8b56ff3813fda120e6c6
rosygit-creator <rosy.github@gmail.com>
2026-01-11 21:49:31 -0800

    rest assured

--- RESTAssuredAutomation/src/test/java/tests/testsjsonschemavalidation.java
+++ RESTAssuredAutomation/src/test/java/tests/testsjsonschemavalidation.java
@@ -1,5 +1,29 @@
+
 package tests;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.hamcrest.Matcher;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class testsjsonschemavalidation {
+	
 
+	
+	@Test
+	public void test_1() {
+		baseURI="https://reqres.in/api";
+		given().
+		get("/users?page=2").
+		then().
+		assertThat().body(matchesJsonSchemaInClasspath("schema.json")).
+		statusCode(200).
+		body("data[1].id", equalTo(8));
+		
+	}
+
+	
+	
 }
